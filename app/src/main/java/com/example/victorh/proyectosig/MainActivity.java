c269b9804a8f674176a02a34ac5ad19af70e9b60
GonzaloAP <gonza.uap@gmail.com>
2018-07-18 10:58:03 -0400

    Funcion de velocidad realizada

--- app/src/main/java/com/example/victorh/proyectosig/MainActivity.java
+++ app/src/main/java/com/example/victorh/proyectosig/MainActivity.java
@@ -1,19 +1,48 @@
 package com.example.victorh.proyectosig;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private EditText edittxtlinea,edittxtplaca,edittxtusuario;
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    private EditText edittxtlinea, edittxtplaca, edittxtusuario;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         iniciarVariable();
+
+        /*Para obtener la velocidad*/
+        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        /*Traer las Actualizciones del GPS*/
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        /*Inicializar en null*/
+        onLocationChanged(null);
+
+        /*Para obtener la velocidad*/
     }
 
     /**
@@ -60,4 +89,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        float v = this.getVelocidad(location);
+        edittxtplaca.setText(v + " km/h ");
+    }
+
+    public float getVelocidad(Location location){
+        float v = 0;
+        if(location == null){
+            System.out.println(v + " km/h ");
+        }else{
+            v = location.getSpeed();
+            v= (float) (v * (3.6));
+            System.out.println(v + " km/h ");
+        }
+        return v;
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
 }
